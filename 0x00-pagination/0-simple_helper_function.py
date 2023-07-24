ee6cde1c9206506dbc5d3a9f58501c5b6a0a64ec
Jabez-abija2399 <jabeznew2399@gmail.com>
2023-07-24 04:26:04 -0700

    0-simple_helper_function.py file updated

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,8 +6,6 @@ def index_range(page, page_size):
         particular pagination parameters. """    
     if page <= 0 or page_size <= 0:
         raise ValueError("Page and page_size should be positive integers.")
-
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
-
     return start_index, end_index - 1  
