ada53701b4027d657b2029ec2d553a9b50a74438
Jabez-abija2399 <jabeznew2399@gmail.com>
2023-07-24 04:27:18 -0700

    Update 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """ Simple helper function """
+
+
 def index_range(page, page_size):
     """ return a tuple of size two containing a start index and an end index
         corresponding to the range of indexes to return in a list for those
-        particular pagination parameters. """    
-    if page <= 0 or page_size <= 0:
-        raise ValueError("Page and page_size should be positive integers.")
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return start_index, end_index - 1  
+        particular pagination parameters. """
+    if page and page_size:
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return start_index, end_index
